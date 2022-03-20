dfe22e735ab50ad587b97ee012366abfaa34a643
dadiletta <dan.adiletta@gmail.com>
2022-03-20 15:00:32 -0400

    new camera code

--- src/main/java/frc/team5973/robot/rapidreact/Robot.java
+++ src/main/java/frc/team5973/robot/rapidreact/Robot.java
@@ -9,6 +9,8 @@ import edu.wpi.first.cscore.CvSource;
 import edu.wpi.first.cscore.MjpegServer;
 import edu.wpi.first.cscore.UsbCamera;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
 import edu.wpi.first.cscore.VideoMode.PixelFormat;
@@ -53,14 +55,58 @@ public class Robot extends RobotBase {
 	private XinputController driver  = new XinputController(driverPort);
 	private XinputController copilot = new XinputController(copilotPort);
 
+	Thread m_visionThread; 
+
 	public Robot() {
 		super("Rapidreact");
+
+
+		m_visionThread =
+        new Thread(
+            () -> {
+              // Get the UsbCamera from CameraServer
+              UsbCamera camera = CameraServer.startAutomaticCapture();
+              // Set the resolution
+              camera.setResolution(640, 480);
+
+              // Get a CvSink. This will capture Mats from the camera
+              CvSink cvSink = CameraServer.getVideo();
+              // Setup a CvSource. This will send images back to the Dashboard
+              CvSource outputStream = CameraServer.putVideo("Rectangle", 640, 480);
+
+              // Mats are very memory expensive. Lets reuse this Mat.
+              Mat mat = new Mat();
+
+              // This cannot be 'true'. The program will never exit if it is. This
+              // lets the robot stop this thread when restarting robot code or
+              // deploying.
+              while (!Thread.interrupted()) {
+                // Tell the CvSink to grab a frame from the camera and put it
+                // in the source mat.  If there is an error notify the output.
+                if (cvSink.grabFrame(mat) == 0) {
+                  // Send the output the error.
+                  outputStream.notifyError(cvSink.getError());
+                  // skip the rest of the current iteration
+                  continue;
+                }
+                // Put a rectangle on the image
+                Imgproc.rectangle(
+                    mat, new Point(100, 100), new Point(400, 400), new Scalar(255, 255, 255), 5);
+                // Give the output stream a new image to display
+                outputStream.putFrame(mat);
+              }
+            });
+			m_visionThread.setDaemon(true);
+			m_visionThread.start();
+		}
 		
+
+		/*
 		// -------------------
-		// CAMERA FEEDS 
-		UsbCamera usbCamera0 = new UsbCamera("cam0", 0);
-		UsbCamera usbCamera1 = new UsbCamera("cam1", 1);
+		// SINGLE CAMERA FEED SETUP WORKS
+		
 		new Thread(() -> {
+			UsbCamera usbCamera0 = CameraServer.startAutomaticCapture();
 			// Creates UsbCamera and MjpegServer [1] and connects them
 			
 			MjpegServer mjpegServer1 = new MjpegServer("serve_USB Camera 0", 1181);
@@ -88,37 +134,10 @@ public class Robot extends RobotBase {
 
 
 		}).start();
-	  
-		
-		new Thread(() -> {
-			// Creates UsbCamera and MjpegServer [1] and connects them
-			MjpegServer mjpegServer1 = new MjpegServer("serve_USB Camera 1", 1181);
-			mjpegServer1.setSource(usbCamera1);
-
-			// Creates the CvSink and connects it to the UsbCamera
-			CvSink cvSink = new CvSink("opencv_USB Camera 1");
-			cvSink.setSource(usbCamera1);
-
-			// Creates the CvSource and MjpegServer [2] and connects them
-			CvSource outputStream = new CvSource("Blur1", PixelFormat.kMJPEG, 640, 480, 30);
-			MjpegServer mjpegServer2 = new MjpegServer("serve_Blur1", 1182);
-			mjpegServer2.setSource(outputStream);
-
-			Mat source = new Mat();
-      		Mat output = new Mat();
-
-			while(!Thread.interrupted()) {
-				if (cvSink.grabFrame(source) == 0) {
-				  continue;
-				}
-				Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
-				outputStream.putFrame(output);
-			}
-		}).start();
-	
 
 		// ---- END CAMERA FEEDS
 		// ----------------------
+		*/
 
         port("shooterWheel",  12);  
 		port("shooterOutake", 8); 
